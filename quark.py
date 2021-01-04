f6f26090b692707a0cb16a7cdffd6691781261ce
AnishLepton <61684782+AnishLepton@users.noreply.github.com>
2021-01-04 21:33:58 +0530

    Create quark.py

---
+++ quark.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+
+client = commands.Bot(commands_prefix = '!')
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+  print('Bot Is Ready')
+  
+@client.command()
+async def hello(message):
+    await message.send('Hi! <@{message.author.id}>')
+    
+client.run('Nzk1NjcwNzIzMzQzMjg2MzQz.X_MwGw.lNxj4zQCGSbG7afmU3o4xcJUSug')
